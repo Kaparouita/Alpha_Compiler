787326ed332dca4d4ff0123e8456b9c0a0e45b05
cst369 <matttsik@gmail.com>
2023-03-22 20:44:26 +0200

    ekana mia klania.parteth

--- Phase2/Symbol_Table.h
+++ Phase2/Symbol_Table.h
@@ -1,37 +1,69 @@
-/*typedef enum types_var {global, typical, local} var_Type;
+typedef enum types_var {global, typical, local} var_Type;
 typedef enum types_func {user_func, lib_func} func_Type;
+
+
+/*** LOG ***
+* den eginan polla, ligo ksekatharisma kai sxolia, exw kollhsei sto pos tha einai h domh symtable.
+*/
 
 
 typedef struct domhvar{
     char* name;
     var_Type var_type;
-    char* scope;
+    int scope;
     int lineno;
+    int hidden = 0;     //me 0 den einai krymmeno, 1 einai 
+    struct* var_struct next_same_scope;
 } var_struct;
 
 
 typedef struct domhfunc{
     char* name;
     func_Type func_type;
-    //prepei na mpoun ta orismata, de jkserw types oute plithos
+    
+    /*  prepei na mpoun ta orismata, pou apo oti fantazomai
+    *   einai apla          var_struct me ->var_type == typical;
+    */
+
     int scope;
     int lineno;
+    int hidden = 0;     //me 0 den einai krymmeno, 1 einai 
+    struct* func_struct next_same_scope;
 } func_struct;
 
 
-void insert(){
-    //
+int insert(){
+    return 0;
 }
 
-void lookup(){
-    //ksekiname apo front3@14 3 bullet  scope link giati an kanw iterate to hash 
-    //table tha mporw na brw ta idia scopes se alla stoixeio xwris na to psaksw olo to table.
-    if(temp->scope == target_scope)
+
+int lookup(char* key_name){
+    return 0;
 }
 
-void hide (int target_scope){
+/*  front3@16 leei na kanoume mia lookup gia sygkekrimeno scope, kai mia oxi*/
+int lookup(char* key_name, int key_scope){
+    
+    /*  find a way to iterate through the hashtable and the whole 
+    *   collision lists until the first symbol with target_scope is found.
+    *   after that iterate through it's scope link
+    *   and hide every node.
+    */
+
+    //temp = 
+    while(temp != NULL){    /*Simple search*/
+        if((strcmp(temp->name,key_name) == 0) && temp->scope == target_scope){
+            //epishs prepei na frontisw na prospelasw oloklhra ta buckets (dhladh kai ta collision lists tous) 
+            //ki edw arxizw na kanw iterate th lista me ta idia scopes.kapws.
+        }
+        temp = temp->next;
+    }
+}
+
+
+int hide (int target_scope){
     //prepei na brw kai sta 2 struct 
     //ta elements me scope == target
-    //iterate kai bres to scope
-    var_struct temp;
-}*/
+    // tha xrhsimopoihthei lookup
+    return 0;
+}
